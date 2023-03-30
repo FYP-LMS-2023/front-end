6444b80e60543387d1f09c7679da317604f9cad3
Shaheer Ahmed <shaheer.ahmed12@gmail.com>
2023-03-31 00:57:15 +0500

    Cards/Headers/Colors/other widgets
    Defined the colours used in the application.
    
    Changed the implementation of the headers and made them more generic
    (this still needs more working)
    
    Added a new header widget for the course overview page headers.
    
    Started working on the progress indicator widget to display the progress of the students
    
    Two cards in the the cards.dart file are incomplete as they are not performing the way they should be. This should be looked at further down the line.
    
    The old cards.dart has been moved to the deprecated file to indicate the widgets created within it are no longer being used in the application.

--- lib/utils/widgets/headers.dart
+++ lib/utils/widgets/headers.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:front_end/constants/fonts.dart';
 
-class TestHeader extends StatelessWidget implements PreferredSizeWidget {
+class ProfileHeader extends StatelessWidget implements PreferredSizeWidget {
   final String name;
   final String id;
 
-  const TestHeader({super.key, required this.name, required this.id});
+  const ProfileHeader({super.key, required this.name, required this.id});
 
   @override
   Widget build(BuildContext context) {
@@ -40,5 +40,32 @@ class TestHeader extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class CourseHeader extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final Function onMenuPressed;
+
+  const CourseHeader(
+      {super.key, required this.title, required this.onMenuPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      centerTitle: false,
+      leading: IconButton(
+        color: Colors.black,
+        icon: const Icon(Icons.menu),
+        onPressed: onMenuPressed as void Function()?,
+      ),
+      title:
+          Text(title, style: Styles.titleMedium.copyWith(color: Colors.black)),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
