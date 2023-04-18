1530518e3e819f8ba1f697fdbd96e69f82935b05
Shaheer Ahmed <shaheer.ahmed12@gmail.com>
2023-04-18 21:11:44 +0500

    Fix: removed redundant imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:front_end/utils/screens/login_page.dart';
-import 'package:front_end/utils/widgets/buttons.dart';
-import 'package:front_end/utils/widgets/headers.dart';
-import 'package:front_end/utils/widgets/cards.dart';
 import '../constants/colors.dart';
-import '../models/mock_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,22 +19,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   bool pressed = false;
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-//         appBar: ProfileHeader(id: "18635", name: "Shaheer Ahmed"),
-//         body: LoginPage());
-//   }
-// }
